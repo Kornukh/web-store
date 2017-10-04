ce1673d7361a2afa7b6766c78ba297df2849af64
Kornukh <v.kornukh@gmail.com>
2017-10-04 11:35:08 -0700

    started the third page

--- app/js/init.js
+++ app/js/init.js
@@ -65,5 +65,13 @@ $( function() {
         range.slider( "option", "values", [ values[0] , this.value ] );
 
     });
-
+    // flexslide init
+    $(window).load(function() {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            controlNav: "thumbnails"
+        });
+    });
+    //Formstyler init
+    $('.input-quantity').styler();
 } );
